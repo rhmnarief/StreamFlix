f10db62072cc1f40f79e6574ac1f47a3bba46c52
Rahman Arief <rhmnarief8@gmail.com>
2021-10-22 00:02:38 +0700

    make commponent,views,routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    <div class="container">
+      <div class="row">
+        <div class="col d-flex align-items-center justify-content-center">
+          <h1>Ini adalah navbar</h1>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
