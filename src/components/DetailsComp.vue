246a990d4c25703814571cdb9b63db54eb24dcfa
Rahman Arief <rhmnarief8@gmail.com>
2021-10-22 23:57:17 +0700

    update styling on movie with details comp

--- src/components/DetailsComp.vue
+++ src/components/DetailsComp.vue
@@ -1,36 +1,36 @@
 <template>
-  <b-jumbotron>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="img">
-            <img src="@/assets/images/Poster-Joker2.jpg" class="w-64" alt="">
-        </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="trailer embed-responsive embed-responsive-16by9">
+        <iframe
+          class="trailer embed-responsive-item"
+          src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+          allowfullscreen
+        ></iframe>
       </div>
-      <div class="col-md-6">
-        <div class="details">
-          <h1>Judul</h1> 
-          <p>82 % | 2019-10-02 Crime, Thriler, Drama </p>
-          <p class="desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam orci
-            metus, luctus ornare rutrum nec, mattis a lectus. Pellentesque
-            vehicula odio ex, eu sollicitudin justo iaculis sit amet. Donec non
-            pellentesque neque. Integer a ultrices felis. Nulla eget ipsum
-            imperdiet, rhoncus est rhoncus, ultricies justo. Sed ultricies
-            fermentum lacus. Integer lectus odio, ultricies vitae dolor vitae,
-            sollicitudin elementum massa
-          </p>
-          <p>Feature cast</p>
-          <div class="actor">
-
-          </div>
-          <div class="button d-flex align-items-center justify-content-center">
-              <button class="btn btn-sucess">Play Trailer</button>
-              <button class="btn btn-danger">Favorite</button>
-          </div>
+      <br />
+      <div class="description">
+        <h1>Judul</h1>
+        <p>82 % | 2019-10-02 Crime, Thriler, Drama</p>
+        <p class="desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam orci
+          metus, luctus ornare rutrum nec, mattis a lectus. Pellentesque
+          vehicula odio ex, eu sollicitudin justo iaculis sit amet. Donec non
+          pellentesque neque. Integer a ultrices felis. Nulla eget ipsum
+          imperdiet, rhoncus est rhoncus, ultricies justo. Sed ultricies
+          fermentum lacus. Integer lectus odio, ultricies vitae dolor vitae,
+          sollicitudin elementum massa
+        </p>
+        <p>Feature cast</p>
+        <div class="actor"></div>
+        <div class="button d-flex align-items-center justify-content-space">
+          <button class="btn btn-success">Play Trailer</button>
+          <div class="m-3"></div>
+          <button class="btn btn-danger">Favorite</button>
         </div>
       </div>
     </div>
-  </b-jumbotron>
+  </div>
 </template>
 
 <script>
@@ -41,4 +41,7 @@ export default {
 </script>
 
 <style>
+.trailer iframe{
+  border-radius: 10px !important ;
+}
 </style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="container">
-    <div class="embed-responsive embed-responsive-16by9">
-      <iframe
-        class="embed-responsive-item"
-        src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
-        allowfullscreen
-      ></iframe>
-    </div>
+   
     <DetailsComp />
   </div>
 </template>
